db90489d759ed178d810c598c1ecb5668014171f
ChoYG <39506854+ygcho-git@users.noreply.github.com>
2023-03-20 22:47:33 +0900

    Create built-in functiion.py

---
+++ built-in functiion.py
@@ -0,0 +1,18 @@
+# 파이썬 내장함수(Built-in Function)
+x = 2.782
+
+print( int(x) )
+print( abs(-15) )
+
+print( round(0.3456, 1), round(0.3456, 2) )
+
+print( max(15, 7, 16, 3, 8) )
+print( min(10, -7, 6, 32) )
+
+print( eval("2 * int(x) + 3") )
+print( len("Hello World") )
+#print( len(x) )
+print( len( str(x) ) )
+
+print( ord('A'), ord('\n') )
+print( chr(97), chr(44032) )
